b304a381c1652c934400e03f76d340c40b3b9587
Samuel Palmér <samiskramis@gmail.com>
2021-10-05 14:45:08 +0200

    Good Commit, got the csv to work

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Quiz
 {
@@ -6,7 +8,34 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random ran = new Random();
+            int points = 0;
+            Console.WriteLine("Välkommen till mitt quiz!");
+            Console.WriteLine("Svara med 1-3 på tangentbordet, försök få så många poäng som möjligt!");
+
+            Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                bool correct = false;
+                int question = ran.Next(1, 32);
+
+
+                if (correct)
+                {
+                    points++;
+                }
+            }
+        }
+        static void mess()
+        {
+            string[] contents = File.ReadAllLines(@"..\qna.csv");
+            List<string[]> LineContents = new List<string[]>();
+            foreach (string c in contents)
+            {
+                LineContents.Add(c.Split(","));
+            }
+            string[][] qna = LineContents.ToArray();
         }
     }
 }
